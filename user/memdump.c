22c917b34f391b8d34c8cf2860626aa6fab295a6
babibasah <babibasah@github.com>
2026-08-08 20:12:07 +0800

    lab-util(memdump): implement memdump

--- user/memdump.c
+++ user/memdump.c
@@ -4,9 +4,7 @@
 
 void memdump(char *fmt, char *data);
 
-int
-main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   if(argc == 1){
     printf("Example 1:\n");
     int a[2] = { 61810, 2025 };
@@ -17,12 +15,12 @@ main(int argc, char *argv[])
     
     printf("Example 3:\n");
     char *s = "another";
-    memdump("s", (char *) &s);
+    memdump("s", (char*) &s);
 
     struct sss {
       char *ptr;
       int num1;
-      short num2;
+      short num2; 
       char byte;
       char bytes[8];
     } example;
@@ -57,9 +55,49 @@ main(int argc, char *argv[])
   exit(0);
 }
 
-void
-memdump(char *fmt, char *data)
-{
-  // Your code here.
+void memdump(char *fmt, char *data) {
+  if (fmt == 0 || data == 0L || *fmt == '\0') {
+    fprintf(2, "both fmt and data must have values");
+    exit(1);
+  }
 
+  while (*fmt != '\0') {
+    switch (*fmt) {
+      case 'i': {
+        int *output = (int*) data;
+        printf("%d\n", *output);
+        data += 4;
+        break;
+      }
+      case 'p': {
+        int val = *(int*) data;
+        printf("%x\n", val);
+        data += 8;
+        break;
+      }
+      case 'h': {
+        short *output = (short*) data;
+        printf("%d\n", *output);
+        data += 2;
+        break;
+      }
+      case 'c': {
+        printf("%c\n", *data);
+        data++;
+        break;
+      }
+      case 's': {
+        char* target_data = *(char**) data;
+        printf("%s\n", target_data);
+        data += 8;
+        break;
+      }
+      case 'S': {
+        printf("%s\n", data);
+        data += strlen(data) + 1;
+        break;
+      }
+    }
+    fmt++;
+  }
 }
