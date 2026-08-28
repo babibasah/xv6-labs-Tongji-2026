41c05f16ed64b411a506a85852fc77a6e8dbe9ea
babibasah <babibasah@github.com>
2026-08-28 22:34:23 +0800

    lab-cow(uvmcopy): modify uvmcopy() to map the parent's physical pages into the child

--- kernel/vm.c
+++ kernel/vm.c
@@ -299,7 +299,6 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
   pte_t *pte;
   uint64 pa, i;
   uint flags;
-  char *mem;
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
@@ -308,13 +307,17 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
       continue;   // physical page hasn't been allocated
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
+
+    if(flags & PTE_W) {
+      flags = (flags & ~PTE_W) | PTE_COW;
+      *pte = (*pte & ~PTE_W) | PTE_COW;
+    }
+
+    if(mappages(new, i, PGSIZE, pa, flags) != 0) {
       goto err;
     }
+
+    incref(pa);
   }
   return 0;
 
